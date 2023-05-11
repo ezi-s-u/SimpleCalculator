d070bd8e2015aa4d2fc5971f9ab986cf7695a57f
ezi-s-u <ljsgoo0617@gmail.com>
2023-05-11 19:30:00 +0900

    계산기 완성

--- app/src/main/java/kr/hs/emirim/evie/simplecalculator/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/evie/simplecalculator/MainActivity.kt
@@ -9,16 +9,48 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     lateinit var edit1 : EditText;
     lateinit var edit2 : EditText;
-    lateinit var btnPlus : Button;
+    lateinit var btnAdd : Button;
     lateinit var btnMul : Button;
     lateinit var btnMinus : Button;
     lateinit var btnDivide : Button;
     lateinit var textResult : TextView;
     lateinit var num1 : String;
     lateinit var num2 : String;
-    var result : Int? = null
+    var result : Int? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        edit1 = findViewById(R.id.edit1)
+        edit2 = findViewById(R.id.edit2)
+        textResult = findViewById(R.id.text_result)
+        btnAdd = findViewById(R.id.btn_plus)
+        btnMinus = findViewById(R.id.btn_minus)
+        btnMul = findViewById(R.id.btn_multiply)
+        btnDivide = findViewById(R.id.btn_divide)
+
+        btnAdd.setOnClickListener {
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) + Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result
+        }
+        btnMinus.setOnClickListener {
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) - Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result
+        }
+        btnMul.setOnClickListener {
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) * Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result
+        }
+        btnDivide.setOnClickListener {
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+            result = Integer.parseInt(num1) / Integer.parseInt(num2)
+            textResult.text = "계산 결과 : " + result
+        }
     }
 }
